072031a9f7533f605a3d62d74a494eb6c644e03a
Satoshi Sahara <sahara.satoshi@gmail.com>
2014-08-15 20:46:30 +0900

    add lang file for config manager

---
+++ lang/en/settings.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * DokuWiki plugin Encrypted Passwords;
+ * English language for settings
+ */
+
+$lang['reload_seconds'] = 'Automatic page reload timeout (in seconds) after decryption to get back encrypted again. Set 0 to disable auto-reload.';
---
+++ lang/ja/settings.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * DokuWiki plugin Encrypted Passwords;
+ * Japanese language for settings
+ */
+
+$lang['reload_seconds'] = '暗号解除後、再度暗号化状態に戻すためにページをリロードするまでの秒数. 0 を指定すると自動ではリロードしません.';
