a57fb41b7b1e49de413e4d6ff39537447d8de180
crash5 <crash5@users.noreply.github.com>
2015-03-26 19:38:54 +0100

    added hungarian language files

---
+++ lang/hu/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['toolbar_icon_title'] = 'Kiválasztott szöveg titkosítása/visszafejtése';
+$lang['decrypt'] = 'Visszafejtés';
+
+$lang['js']['enterKey']    = 'Adja meg a kulcsot:';
+$lang['js']['decrypt']     = 'Visszafejtés';
+$lang['js']['encrypt']     = 'Titkosítás';
+$lang['js']['cancel']      = 'Mégse';
+$lang['js']['encryptKey']  = 'Titkosító kulcs megadása kétszer:';
+$lang['js']['keyErr']      = 'A két kulcs nem egyezik meg!';
+$lang['js']['emptyKey']    = 'Adjon meg egy kulcsot!';
+$lang['js']['invalidKey']  = 'Hibás kulcs';
+$lang['js']['noSelection'] = 'Válassza ki a titkosítandó szöveget';
+$lang['js']['recrypt']     = 'újra titkosítás';
