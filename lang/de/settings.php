fa38de30430ae82d38c0fe611e2d79927005b35e
geofreak <info@geofreak.de>
2016-01-17 11:49:23 +0000

    Update german language

---
+++ lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * DokuWiki plugin Encrypted Passwords;
+ * German language for settings
+ */
+
+$lang['reload_seconds'] = 'Automatisches neuladen der Seite in Sekunden nach der Entschlüsselung. Durch das Neuladen der Seite, ist der Text wieder verschlüsselt. Verwendet man 0, wird das automatische neuladen der Seite deaktiviert.';
+$lang['notify']         = 'Benachrichtigung, wenn das automatische Speichern des Entwurfes deaktiviert wird.';
