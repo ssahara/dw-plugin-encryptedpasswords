25f2a6e51ead42d456329cc51603cd2e8bcec9d0
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-04-28 15:16:53 +0900

    Fix Japanese lang for decrypt
    「暗号化」の反対は「復号化」ではない

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -6,17 +6,17 @@
  * @author     Satoshi Sahara <sahara.satoshi@gmail.com>
  */
 
-$lang['toolbar_icon_title'] = '選択テキストの暗号化または復号化';
-$lang['decrypt'] = '復号化';
+$lang['toolbar_icon_title'] = '選択テキストの暗号化または復号';
+$lang['decrypt'] = '復号';
 
 $lang['js']['enterKey']    = '復号キーを入力:';
-$lang['js']['decrypt']     = '復号化';
+$lang['js']['decrypt']     = '復号';
 $lang['js']['encrypt']     = '暗号化';
 $lang['js']['cancel']      = 'キャンセル';
-$lang['js']['encryptKey']  = '暗号化／復号化に使用するキーを入力してください.<br />同じキーを２度入力します:';
+$lang['js']['encryptKey']  = '暗号化／復号に使用するキーを入力してください.<br />同じキーを２度入力します:';
 $lang['js']['keyErr']      = '２つにキーが同一でありません.<br />同じキーを２度入力してください!';
 $lang['js']['emptyKey']    = 'キーを入力してください!';
-$lang['js']['invalidKey']  = '復号化キーが不正です';
+$lang['js']['invalidKey']  = '復号キーが不正です';
 $lang['js']['noSelection'] = '暗号化するテキストを選択してください';
 $lang['js']['recrypt']     = '暗号状態に戻す';
 
