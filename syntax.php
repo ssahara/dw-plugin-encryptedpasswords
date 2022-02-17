6199bd5cc7a9b2c0f208ca5027424ca211a07cb7
Andreas Gohr <andi@splitbrain.org>
2022-02-17 09:32:18 +0100

    simplified syntax
    Most functionality will be added via JavaScript

--- syntax.php
+++ syntax.php
@@ -1,98 +1,47 @@
 <?php
 
 /**
-  * Encrypted Passwords Plugin: Store encrypted passwords with syntax <decrypt></decrypt>
-  *
-  * @license    GPL2 (http://www.gnu.org/licenses/gpl.html)
-  * @author     Wolfgang Reszel <reszel@werbeagentur-willers.de>
-  */
-class syntax_plugin_encryptedpasswords extends DokuWiki_Syntax_Plugin
+ * Encrypted Passwords Plugin: Store encrypted passwords with syntax <decrypt></decrypt>
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ */
+class syntax_plugin_encryptedpasswords extends \dokuwiki\Extension\SyntaxPlugin
 {
+    /** @inheritDoc */
     public function getType()
-    {   // Syntax Type
-        return 'protected';
-    }
-
-    /**
-      * Connect pattern to lexer
-      */
-    protected $mode, $pattern;
-
-    public function preConnect()
     {
-        // syntax mode, drop 'syntax_' from class name
-        $this->mode = substr(get_class($this), 7);
-        // syntax pattern
-        $this->pattern = array(
-            1 => '<decrypt>(?=.*?</decrypt>)',  // DOKU_LEXER_ENTER
-            4 => '</decrypt>',                  // DOKU_LEXER_EXIT
-        );
+        return 'substition';
     }
 
-    public function connectTo($mode)
-    {
-        $this->Lexer->addEntryPattern($this->pattern[1], $mode, $this->mode);
-    }
-
-    public function postConnect()
-    {
-        $this->Lexer->addExitPattern($this->pattern[4], $this->mode);
-    }
-
+    /** @inheritDoc */
     public function getSort()
-    {   // sort number used to determine priority of this mode
+    {
         return 65;
     }
 
-    /**
-      * Handle the match
-      */
+    /** @inheritDoc */
+    public function connectTo($mode)
+    {
+        $this->Lexer->addSpecialPattern('<decrypt>(?:.*?<\/decrypt>)', $mode, 'plugin_encryptedpasswords');
+    }
+
+    /** @inheritDoc */
     public function handle($match, $state, $pos, Doku_Handler $handler)
     {
-        switch ($state) {
-            case DOKU_LEXER_ENTER :
-                return $data = array($state, '');
-                break;
-            case DOKU_LEXER_UNMATCHED :
-                return $data = array($state, $match);
-                break;
-            case DOKU_LEXER_EXIT :
-                return $data = array($state, '');
-                break;
-        }
-        return $data = false;
+        $crypt = substr($match, 9, -10); // remove tags
+        return [$crypt];
     }
 
-    /**
-      * Create output
-      */
-    public function render($format, Doku_Renderer $renderer, $data)
+    /** @inheritDoc */
+    public function render($mode, Doku_Renderer $renderer, $data)
     {
-        if ($format == 'xhtml') {
-
-            list($state, $match) = $data;
-            switch ($state) {
-                case DOKU_LEXER_ENTER :
-                    $renderer->doc.= '';
-                    break;
-                case DOKU_LEXER_UNMATCHED :
-                    $id = uniqid();
-                    $script = "decryptText(jQuery('.encryptedpasswords'));";
-                    if ($this->getConf('reload_seconds') > 0) {
-                        $script.= "window.setTimeout('location.reload()',"
-                                 .$this->getConf('reload_seconds')."000);";
-                    }
-                    $renderer->doc.= '<span class="encryptedpasswords" title="'.$match.'">';
-                    $renderer->doc.= '<a title="'.$this->getLang('decrypt').'" onclick="'
-                                    .$script.'">••••••••••</a>';
-                    $renderer->doc.= '</span>';
-                    break;
-                case DOKU_LEXER_EXIT :
-                    $renderer->doc .= '';
-                    break;
-            }
-            return true;
+        if ($mode !== 'xhtml') {
+            return false;
         }
-        return false;
+
+        $crypt = hsc($data[0]);
+        $renderer->doc .= '<span class="encryptedpasswords crypted" data-crypted="' . $crypt . '">••••••••••</span>';
+        return true;
     }
 }
+
