e6f93edae617aa5a805e74175919db480ebfdb0a
Andreas Gohr <andi@splitbrain.org>
2022-02-17 17:19:30 +0100

    allow for decoding passwords in the editor

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// register toolbar button
+if (typeof window.toolbar !== 'undefined') {
+    toolbar[toolbar.length] = {
+        type: 'encryptedPasswordsToggle',
+        title: 'FIXME button title',
+        icon: DOKU_BASE + 'lib/plugins/encryptedpasswords/encrypt.png',
+    };
+
+    // actual click handler is defined in the EditorHandling class later
+    function addBtnActionEncryptedPasswordsToggle($btn, props, edid) {
+        $btn.on('click', function () {
+            $btn.trigger('EncryptedPasswordsToggleEvent', [edid]);
+        })
+    }
+}
+
 jQuery(function () {
     /* DOKUWIKI:include script/SubtleAES.js */
     /* DOKUWIKI:include script/PageHandling.js */
--- script/EditorHandling.js
+++ script/EditorHandling.js
@@ -1,4 +1,7 @@
 class EditorHandling {
+
+    failcount = 0;
+
     /**
      * Register handlers
      *
@@ -8,10 +11,74 @@ class EditorHandling {
         this.aes = aes;
 
         jQuery('#edbtn__save, #edbtn__preview').on('click', this.encryptAllSyntax.bind(this));
+        jQuery(document).on('EncryptedPasswordsToggleEvent', this.decryptAllSyntax.bind(this));
     }
 
     /**
      *
+     * @param {Event} e
+     * @param {string} edid The editor's textarea ID
+     * @return {Promise<void>}
+     */
+    async decryptAllSyntax(e, edid) {
+        const form = jQuery(`#${edid}`)[0].form;
+        // access the syntax in the editor
+        const prefix = form.prefix.value;
+        const suffix = form.suffix.value;
+        const text = form.wikitext.value;
+
+        // check if syntax contains encrypted passwords
+        const re = new RegExp('<decrypt>.*?(<\/decrypt>)');
+        if (
+            !prefix.match(re) &&
+            !suffix.match(re) &&
+            !text.match(re)
+        ) {
+            return;
+        }
+
+        // ask for passphrase
+        const passphrase = window.prompt("Please enter the passphrase");
+        if (passphrase === null || passphrase === '') return;
+
+        // replace
+        this.failcount = 0;
+        form.prefix.value = await this.decryptSyntax(prefix, passphrase);
+        form.suffix.value = await this.decryptSyntax(suffix, passphrase);
+        form.wikitext.value = await this.decryptSyntax(text, passphrase);
+
+        if(this.failcount) {
+            alert(`${this.failcount} passwords not decoded`);
+        }
+    }
+
+    /**
+     * Encrypt all clear text syntax in the given text
+     *
+     * @param {string} text
+     * @param {string} passphrase
+     * @return {Promise<string>}
+     */
+    async decryptSyntax(text, passphrase) {
+        const re = new RegExp('<decrypt>(.*?)(<\/decrypt>)', 'g');
+
+        const matches = [...text.matchAll(re)];
+
+        for (let i = 0; i < matches.length; i++) {
+            const cipher = matches[i][1];
+            try {
+                const clear = await this.aes.aesGcmDecrypt(cipher, passphrase);
+                text = text.replace(matches[i][0], `<encrypt>${clear}</encrypt>`);
+            } catch (e) {
+                this.failcount++;
+            }
+        }
+
+        return text;
+    }
+
+    /**
+     * Event handler to encrypt all passwords
      *
      * @param {Event} e
      * @return {Promise<void>}
