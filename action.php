6646d5458f2e2dc810e8cc53bb05e2849fc26e77
Andreas Gohr <andi@splitbrain.org>
2022-02-21 13:39:39 +0100

    prevent password leaks via drafts

--- action.php
+++ action.php
@@ -12,6 +12,7 @@ class action_plugin_encryptedpasswords extends \dokuwiki\Extension\ActionPlugin
     public function register(Doku_Event_Handler $controller)
     {
         $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, 'handleDokuWikiStarted');
+        $controller->register_hook('DRAFT_SAVE', 'BEFORE', $this, 'handleDraftSave');
 
     }
 
@@ -28,5 +29,21 @@ class action_plugin_encryptedpasswords extends \dokuwiki\Extension\ActionPlugin
         $JSINFO['plugins']['encryptedpasswords']['timeout'] = $this->getConf('reload_seconds');
     }
 
+    /**
+     * Remove any unencrypted passwords from the drafts
+     *
+     * @param Doku_Event $event event object by reference
+     * @param mixed $param optional parameter passed when event was registered
+     * @return void
+     */
+    public function handleDraftSave(Doku_Event $event, $param)
+    {
+        $re = '/<encrypt>.*?(<\/encrypt>)/';
+        $repl = '[password removed from draft]';
+
+        $event->data['prefix'] = preg_replace($re, $repl, $event->data['prefix']);
+        $event->data['suffix'] = preg_replace($re, $repl, $event->data['suffix']);
+        $event->data['text'] = preg_replace($re, $repl, $event->data['text']);
+    }
 }
 
